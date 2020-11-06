c252402358c9cce92c2e37f60d4d9a25853e1191
kpac567 <71659832+kpac567@users.noreply.github.com>
2020-11-06 14:39:21 -0500

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -5,11 +5,11 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: "localhost",
+    host: "cig4l2op6r0fxymw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	",
     port: 3306,
-    user: "root",
-    password: "",
-    database: "burgers_db"
+    user: "ifhdf6gevvhbcgnv",
+    password: "ei0n310cz5p1wofc",
+    database: "qxpes46u2a8i2tdc"
   });
 };
 
